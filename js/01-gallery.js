4edcfd2a509729a26d29d218d70b6e564f075414
MichalJurek <jurekmichal@o2.pl>
2023-03-12 20:29:04 +0100

    wrzucenie plików do układu

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery")
+galleryItems.forEach((item) => {
+    const galleryItem = `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`;
+  gallery.insertAdjacentHTML("afterend", galleryItem);
+});
 
 console.log(galleryItems);
