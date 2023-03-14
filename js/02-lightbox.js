7aefdb50ae1a4e9440c042dd34c7685dbf4aa340
MichalJurek <jurekmichal@o2.pl>
2023-03-14 15:24:47 +0100

    podpiecie kodu z js 1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery")
+galleryItems.forEach((item) => {
+    const galleryItem = `<div class="gallery__item">
+        <a class="gallery__item" href="${item.original}">
+            <img
+                class="gallery__image"
+                src="${item.preview}"
+                alt="${item.description}"
+            />
+        </a>
+    </div>`;
+    gallery.insertAdjacentHTML("beforeend", galleryItem);
+});
+document.querySelector(".gallery").onclick = (event) => {
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="1400" height="900">
+  `)
+    instance.show();
+};
+
 
 console.log(galleryItems);
