7f8125a9568625bc2bf2d6a6bdc94e7b21499b40
MichalJurek <jurekmichal@o2.pl>
2023-03-14 15:00:25 +0100

    okno modalne + otwarcie obrazka

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector(".gallery")
 galleryItems.forEach((item) => {
-    const galleryItem = `<div class="gallery__item">
+  const galleryItem = `<div class="gallery__item">
     <a class="gallery__link" href="${item.original}">
       <img
         class="gallery__image"
@@ -14,5 +14,11 @@ galleryItems.forEach((item) => {
   </div>`;
   gallery.insertAdjacentHTML("beforeend", galleryItem);
 });
-
+document.querySelector(".gallery").onclick = (event) => {
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="1400" height="900">
+`)
+  instance.show();
+}
 console.log(galleryItems);
