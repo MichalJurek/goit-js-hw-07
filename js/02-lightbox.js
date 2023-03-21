10566296b16d16d6e46d48dd74f5a8486fdda23e
MichalJurek <jurekmichal@o2.pl>
2023-03-21 14:43:30 +0100

    poprawki

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,9 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector(".gallery")
+const gallery = document.querySelector(".gallery");
 galleryItems.forEach((item) => {
-    const galleryItem = `<div class="gallery__item">
+    const galleryItem =
+    `<div class="gallery__item">
         <a class="gallery__item" href="${item.original}">
             <img
                 class="gallery__image"
@@ -11,24 +12,15 @@ galleryItems.forEach((item) => {
             />
         </a>
     </div>`;
-    gallery.insertAdjacentHTML("beforeend", galleryItem);
+	gallery.insertAdjacentHTML("beforeend", galleryItem);
 });
-document.querySelector(".gallery").onclick = (event) => {
-    event.preventDefault();
-    const instance = basicLightbox.create(`
-      <img src="${event.target.dataset.source}" width="1400" height="900">
-  `)
-    instance.show();
-};
-
-const lightbox = new SimpleLightbox(".gallery a", {
-    captions: true,
-    captionSelector: "img",
-    captionType: "attr",
-    captionsData: "alt",
-    captionPosition: "bottom",
-    captionDeley: 250,
-  });
-
+const lightbox = new SimpleLightbox('.gallery a', {
+	captions: true,
+	captionSelector: "img",
+	captionType: "attr",
+	captionsData: "alt",
+	captionPosition: "below",
+	captionDeley: 250,
+});
 
 console.log(galleryItems);
