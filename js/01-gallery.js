a493fc2a74b75346ac2f8d00bdf508b1799735fc
Michał <jurekmichal@o2.pl>
2023-11-17 21:42:28 +0100

    Update 01-gallery.js
    poprawka ppkt 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
 galleryItems.forEach((item) => {
-  const galleryItem = `<div class="gallery__item">
+	const galleryItem = `<div class="gallery__item">
     <a class="gallery__link" href="${item.original}">
       <img
         class="gallery__image"
@@ -12,12 +12,15 @@ galleryItems.forEach((item) => {
       />
     </a>
   </div>`;
-  gallery.insertAdjacentHTML("beforeend", galleryItem);
+	gallery.insertAdjacentHTML("beforeend", galleryItem);
 });
 document.querySelector(".gallery").onclick = (event) => {
-  event.preventDefault();
-  const instance = basicLightbox.create(`
+	event.preventDefault();
+	if (event.target.classList.contains("gallery__image")) {
+		// dodanie warunku if spowoduje, że w zdarzenie (kliknięcie) w elemencie z klasą "gallery__image" [w linijce 8 definiowaliśm tę klasę] zadziała
+		const instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}" width="1400" height="900">`);
-  instance.show();
+		instance.show();
+	}
 };
 console.log(galleryItems);
