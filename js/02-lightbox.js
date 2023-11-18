1e97fd093109f5f12c878596636fad22248517de
Michał <jurekmichal@o2.pl>
2023-11-18 13:25:33 +0100

    Update 02-lightbox.js
    mamy właściwą strukturę ale nie mamy stylowania miniatur

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,24 +2,33 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
 galleryItems.forEach((item) => {
-  const galleryItem = `<div class="gallery__item">
-        <a class="gallery__item" href="${item.original}">
-            <img
-                class="gallery__image"
-                src="${item.preview}"
-                alt="${item.description}"
-            />
-        </a>
-    </div>`;
-  gallery.insertAdjacentHTML("beforeend", galleryItem);
+	const galleryItem = `
+  <li>
+    <a class="gallery__item" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>
+  `;
+
+	gallery.insertAdjacentHTML("beforeend", galleryItem);
 });
 const lightbox = new SimpleLightbox(".gallery a", {
-  captions: true,
-  captionSelector: "img",
-  captionType: "attr",
-  captionsData: "alt",
-  captionPosition: "below",
-  captionDeley: 250,
+	captions: true,
+	captionSelector: "img",
+	captionType: "attr",
+	captionsData: "alt",
+	captionPosition: "below",
+	captionDelay: 250,
 });
 
 console.log(galleryItems);
+
+/* wymagana struktura
+<a class="gallery__item" href="large-image.jpg">
+	<img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a>;
+*/
